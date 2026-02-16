d7b8a1b6f6bde96a16bd41e3295408b697447a53
prabha-2 <prabhagurunaathan@gmail.com>
2026-02-16 17:24:46 +0530

    Final Terraform setup with Docker image variables

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -23,19 +23,26 @@ resource "aws_security_group" "strapi_sg" {
   }
 }
 
-resource "aws_instance" "strapi" {
-  ami           = "ami-0f5ee92e2d63afc18" # Amazon Linux 2 (us-east-1)
+resource "aws_instance" "strapi_ec2" {
+  ami           = "ami-03f4878755434977f"
   instance_type = "t2.micro"
   key_name      = var.key_name
-  security_groups = [aws_security_group.strapi_sg.name]
+
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
               yum install docker -y
               service docker start
-              usermod -aG docker ec2-user
+              usermod -a -G docker ec2-user
+
               docker pull ${var.docker_username}/strapi:${var.image_tag}
               docker run -d -p 1337:1337 --name strapi ${var.docker_username}/strapi:${var.image_tag}
               EOF
+
+  tags = {
+    Name = "Strapi-Server"
+  }
 }
+
